89e5e41b7eece865ba4ac5751c5f9ba6bd5ea44b
omrobbie <putu.roby@yahoo.com>
2020-08-05 16:20:48 +0800

    * start mvvm 001 project *

---
+++ MVVM-001/MVVM-001/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MVVM-001
+//
+//  Created by omrobbie on 05/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
