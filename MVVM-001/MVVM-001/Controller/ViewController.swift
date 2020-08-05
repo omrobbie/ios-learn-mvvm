9c50a3d0b78424a8c5c1257f8469233ab0a467cc
omrobbie <putu.roby@yahoo.com>
2020-08-05 16:50:40 +0800

    run view model in controller

--- MVVM-001/MVVM-001/Controller/ViewController.swift
+++ MVVM-001/MVVM-001/Controller/ViewController.swift
@@ -14,10 +14,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtUsername: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
 
+    var authenticationVM = AuthenticationVM()
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func btnLoginTapped(_ sender: Any) {
+        guard let username = txtUsername.text,
+            let password = txtPassword.text
+            else {return}
+
+        lblInformation.text = "Information"
+        authenticationVM.authenticationUserWith(username, andPassword: password)
+        authenticationVM.loginCompletionHandler { (status, message) in
+            if status {
+                self.lblInformation.text = "Logged in with username = \(self.authenticationVM.username) and email = \(self.authenticationVM.email)"
+            } else {
+                self.lblInformation.text = message
+            }
+        }
     }
 }
