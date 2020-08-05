626280367067d5ec85e87eac1a51d8cb65bfda46
omrobbie <putu.roby@yahoo.com>
2020-08-05 16:29:20 +0800

    create user model

---
+++ MVVM-001/MVVM-001/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  MVVM-001
+//
+//  Created by omrobbie on 05/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+struct User {
+
+    let username: String
+    let email: String
+}
