53a1d2598cf3a5a5c027cbf8dd3fdf995628cc61
omrobbie <putu.roby@yahoo.com>
2020-08-05 16:43:26 +0800

    give login callback from the actions

--- MVVM-001/MVVM-001/ViewModel/AuthenticationVM.swift
+++ MVVM-001/MVVM-001/ViewModel/AuthenticationVM.swift
@@ -10,23 +10,39 @@ import Foundation
 
 class AuthenticationVM: NSObject {
 
+    var user: User!
+
+    var username: String {return user.username}
+    var email: String {return user.email}
+
+    typealias authenticationLoginCallback = (_ status: Bool, _ message: String) -> Void
+    var loginCallback: authenticationLoginCallback?
+
     func authenticationUserWith(_ username: String, andPassword password: String) {
         if username.count != 0 {
             if password.count != 0 {
                 verifyUserWith(username, andPassword: password)
             } else {
                 // empty password
+                loginCallback?(false, "Password should not be empty!")
             }
         } else {
             // empty username
+            loginCallback?(false, "User should not be empty!")
         }
+    }
+
+    func loginCompletionHandler(callback: @escaping authenticationLoginCallback) {
+        loginCallback = callback
     }
 
     fileprivate func verifyUserWith(_ username: String, andPassword password: String) {
         if username == "test" && password == "1234" {
-            let user = User(username: username, email: "\(username)@test.com")
+            user = User(username: username, email: "\(username)@test.com")
+            loginCallback?(true, "User is successfully authenticated!")
         } else {
             // invalid credentials
+            loginCallback?(false, "Please enter the valid credentials!")
         }
     }
 }
