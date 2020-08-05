a83ac3588615918a10ddbd7c56954ce62c30111e
omrobbie <putu.roby@yahoo.com>
2020-08-05 17:14:06 +0800

    prepare view model

---
+++ MVVM-002/MVVM-002/ViewModel/MovieVM.swift
@@ -0,0 +1,16 @@
+//
+//  MovieVM.swift
+//  MVVM-002
+//
+//  Created by omrobbie on 05/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import Foundation
+
+class MovieVM: NSObject {
+
+    func fetchMovies(completion: () -> ()) {
+
+    }
+}
