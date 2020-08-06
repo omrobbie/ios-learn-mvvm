57b5cc7670f7b223fa6be744202c5239af7b2b40
omrobbie <putu.roby@yahoo.com>
2020-08-06 10:01:03 +0800

    * start mvvm 003 project *

---
+++ MVVM-003/MVVM-003/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  MVVM-003
+//
+//  Created by omrobbie on 06/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
---
+++ MVVM-003/MVVM-003/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MVVM-003
+//
+//  Created by omrobbie on 06/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
