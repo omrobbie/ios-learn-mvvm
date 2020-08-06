363746cdef2df8113ee07bb973982c4a7b7f90ec
omrobbie <putu.roby@yahoo.com>
2020-08-06 10:06:24 +0800

    create nice place model

---
+++ MVVM-003/MVVM-003/Model/NicePlace.swift
@@ -0,0 +1,13 @@
+//
+//  NicePlace.swift
+//  MVVM-003
+//
+//  Created by omrobbie on 06/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+struct NicePlace {
+
+    let title: String
+    let imageUrl: String
+}
