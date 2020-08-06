91e301daaca284fab88dbcb0e6bd70e3b677198c
omrobbie <putu.roby@yahoo.com>
2020-08-06 10:32:49 +0800

    prepare view model

---
+++ MVVM-003/MVVM-003/ViewModel/MainVM.swift
@@ -0,0 +1,16 @@
+//
+//  MainVM.swift
+//  MVVM-003
+//
+//  Created by omrobbie on 06/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+class MainVM {
+
+    var nicePlaces = [NicePlace]()
+
+    func getNicePlaces() -> [NicePlace] {
+        return nicePlaces
+    }
+}
