c02aa480784c742f767c53c8fd5676a99491c94d
omrobbie <putu.roby@yahoo.com>
2020-08-07 18:35:43 +0800

    prepare view model

---
+++ MVVM-004/MVVM-004/Presentation/ViewModel/ViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ViewModel.swift
+//  MVVM-004
+//
+//  Created by omrobbie on 07/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+class ViewModel {
+
+    var model = [ModelEntry]()
+
+    func getTitleList() {
+        // TODO: Get data from API
+    }
+}
