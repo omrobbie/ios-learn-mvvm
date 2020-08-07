0b2cda0945cb2d5ccb2eada0bdb5dc1da04e5a0a
omrobbie <putu.roby@yahoo.com>
2020-08-07 16:18:28 +0800

    * start mvvm 004 project *

---
+++ MVVM-004/MVVM-004/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MVVM-004
+//
+//  Created by omrobbie on 07/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
