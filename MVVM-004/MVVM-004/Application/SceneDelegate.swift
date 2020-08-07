e544d63a5478a84959b9f44355b051b374b50ff6
omrobbie <putu.roby@yahoo.com>
2020-08-08 07:03:51 +0800

    assembly all object and run it

--- MVVM-004/MVVM-004/Application/SceneDelegate.swift
+++ MVVM-004/MVVM-004/Application/SceneDelegate.swift
@@ -13,6 +13,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+
+        let restApi = RestApiImpl()
+        let repository = RepositoryImpl(restApi: restApi)
+        let useCase = UseCaseImpl(repository: repository)
+        let viewModel = ViewModel(useCase: useCase)
+        let viewController = ViewController.create(viewModel: viewModel)
+
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
     }
 }
