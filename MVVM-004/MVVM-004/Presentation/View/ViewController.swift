6ec9e55e0692aab4b90b69b6783dc2f16c8065a6
omrobbie <putu.roby@yahoo.com>
2020-08-08 11:26:18 +0800

    setup layout and show data to the list from view model

--- MVVM-004/MVVM-004/Presentation/View/ViewController.swift
+++ MVVM-004/MVVM-004/Presentation/View/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     private var viewModel: ViewModelImpl!
 
     static func create(viewModel: ViewModelImpl) -> UIViewController {
@@ -24,6 +26,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         viewModel.getTitleList().observe = { data in
             self.viewModel.model = data
+            self.tableView.reloadData()
         }
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.model.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let item = viewModel.model[indexPath.row]
+        cell.textLabel?.text = item.title.label
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
