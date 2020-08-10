e3f3f71d27fce43b18b3393e2a11308a7b2470d0
omrobbie <putu.roby@yahoo.com>
2020-08-10 11:53:03 +0800

    add table view with dummy data

--- MVVM-005/MVVM-005/Presentation/View/ViewController.swift
+++ MVVM-005/MVVM-005/Presentation/View/ViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     var viewModel: ViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.getTitleList()
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Item \(indexPath.row)"
+        return cell
     }
 }
