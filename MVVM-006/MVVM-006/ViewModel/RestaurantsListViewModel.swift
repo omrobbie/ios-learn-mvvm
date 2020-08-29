ea8882f25619b44c524315b8cd1650c2a1bb4d3d
omrobbie <putu.roby@yahoo.com>
2020-08-29 12:09:22 +0800

    prepare view model

---
+++ MVVM-006/MVVM-006/ViewModel/RestaurantsListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RestaurantsListViewModel.swift
+//  MVVM-006
+//
+//  Created by omrobbie on 29/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import RxSwift
+
+final class RestaurantsListViewModel {
+    let title = "Restaurants"
+}
